243c2aff35dee2135ea9aa6be0e1bb0884c8c158
Lyryo 06 <yazid.arbi@etu.uca.fr>
2025-12-14 16:17:59 +0100

    ajout projet

---
+++ CalculPourcentage/Components/Pages/Counter.razor.cs
@@ -0,0 +1,34 @@
+﻿namespace CalculPourcentage.Components.Pages
+{
+    public partial class Counter
+    {
+        public decimal Resultat { get; set; } = 0;
+        public decimal ResultatTransport { get; set; } = 0;
+        public decimal Commission { get; set; } = 1;
+        public decimal PrixChariot { get; set; } = 0;
+        public decimal FraisGestion { get; set; } = 1;
+        public decimal NbElt { get; set; } = 0;
+        public decimal PrixAchat { get; set; } = 0;
+
+        public void CalculerTransport()
+        {
+            if (NbElt == 0) ResultatTransport = PrixChariot;
+            else ResultatTransport = Math.Round(PrixChariot / NbElt, 2);
+        }
+        public void Calculer()
+        {
+            CalculerTransport();
+            if (FraisGestion == 0) Resultat = Math.Round(PrixAchat * Commission + ResultatTransport, 2);
+            else Resultat = Math.Round(PrixAchat * Commission * FraisGestion + ResultatTransport, 2);
+        }
+        public void Reinitialiser()
+        {
+            Commission = 1;
+            PrixChariot = 0;
+            PrixAchat = 0;
+            NbElt = 0;
+            FraisGestion = 1;
+            Calculer();
+        }
+    }
+}
---
+++ CalculPourcentage/Model/Calcul.cs
@@ -0,0 +1,7 @@
+﻿namespace CalculPourcentage.Model
+{
+    public class Calcul
+    {
+        
+    }
+}
